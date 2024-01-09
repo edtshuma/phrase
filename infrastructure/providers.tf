6e15e6c7fd3f99c4dc530fe41c6027b3f1b1a4b8
edtshuma <edtshuma@gmail.com>
2024-01-09 09:58:02 +0100

    terraform init

---
+++ infrastructure/providers.tf
@@ -0,0 +1,15 @@
+#--root/providers.tf---
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.15.0"
+    }
+  }
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
